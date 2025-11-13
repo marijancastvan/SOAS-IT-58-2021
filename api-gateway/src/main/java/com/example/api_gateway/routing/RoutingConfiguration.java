66873db5f5ea500bbd7a540a82956833658edd4d
Marijan <castvan.marian@gmail.com>
2025-11-13 12:11:23 +0100

    VEZBE 6 - SOAS: Authentication

--- api-gateway/src/main/java/com/example/api_gateway/routing/RoutingConfiguration.java
+++ api-gateway/src/main/java/com/example/api_gateway/routing/RoutingConfiguration.java
@@ -15,6 +15,7 @@ public class RoutingConfiguration {
 				.route(p -> p.path("/currency-conversion")
 						.filters(f -> f.rewritePath("/currency-conversion", "/currency-conversion-feign"))
 						.uri("lb://currency-conversion"))
+				.route(p -> p.path("/users/**").uri("lb://users-service"))
 				.build();
 	}
 }
